a9fb6d5cf0fd5031f9f678a5fb2bd33889f572c1
Fiona Toublanc <fionatoublanc@gmail.com>
2020-08-26 16:12:37 +0200

    show update

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,6 +1,10 @@
 class DesignersController < ApplicationController
 
-  def show
+
+
+
+
+def show
     @designer = Designer.find(params[:id])
     @products = Product.where(designer: @designer)
   end
