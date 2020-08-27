4755ad4609d0e59c6b5abccb685117722993c56d
Fiona Toublanc <fionatoublanc@gmail.com>
2020-08-27 17:07:09 +0200

    product show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
 
   resources :designers, only: [:index, :show]
 
