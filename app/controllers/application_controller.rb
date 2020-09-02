02fedd78b28b210e6c312cde52d16e0fdb135e8b
manouk van kuyk <manoukvankuyk@gmail.com>
2020-09-02 14:53:11 +0200

    shit is fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user! 
   before_action :current_order
   def current_order
-    @cart = Order.find_by(user: current_user, status: "pending")
+    @cart = Order.find_by(status: "pending")
      @cart_amount = 0
       @cart.order_products.each do |product|
         @cart_amount += product.quantity
