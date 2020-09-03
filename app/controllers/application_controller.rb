944848d4e7775a2b3bd39d4dc997d6b88e34cd71
valerie schraauwers <valerieschraauwers@gmail.com>
2020-09-03 09:44:22 +0200

    Initialize session[:order_id] and @cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :current_order
-  def current_order
-    @cart = Order.find_by(user: current_user, status: "pending")
+  before_action :initialize_session
+  before_action :initialize_cart
+
+  def initialize_cart
+    @cart = Order.find(session[:order_id])
     @cart_amount = 0
     @cart.order_products.each do |product|
       @cart_amount += product.quantity
     end
+  end
+
+  def initialize_session
+    session[:order_id] ||= Order.create(status: "pending").id
   end
 end
 
